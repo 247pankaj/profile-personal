a236f651446c3198824dd1ced258da6aedd31dec
Pankaj <yadavpankaj0248@gmail.com>
2025-09-07 16:15:35 +0200

    Added FORMSPREE_ID changes

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -9,11 +9,17 @@ module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
+    const experiences = JSON.parse(
+        fs.readFileSync(upath.join(srcPath, 'data', 'experiences.json'), 'utf-8')
+    );
+
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath,
+        formspreeId: process.env.FORMSPREE_ID,
+        experiences
     });
 
     const destPathDirname = upath.dirname(destPath);
